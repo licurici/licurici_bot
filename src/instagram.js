596f51025e41ce946b9f5dbdd4406c3d37a2547f
Szabo Bogdan <bogdan.szabo@altom.ro>
2016-06-11 20:45:37 +0300

    Instagram api

---
+++ src/instagram.js
@@ -0,0 +1,44 @@
+var settings = require('../settings.js');
+
+var instagramWrapi = require('@wrapi/instagram');
+
+var client = new instagramWrapi(settings.instagramToken);
+var count = 0;
+
+
+function init(serial, bot) {
+  client.tags.get(settings.hashTag, function(err, data) {
+    if (!err) {
+      var message = "pe instagram sunt " + data.data.media_count + " poze cu #" + settings.hashTag + "\n";
+      message += "https://www.instagram.com/explore/tags/" + settings.hashTag + "\n";
+
+      count = data.data.media_count;
+      bot.beginDialog({channel: settings.slackChannel}, '/notify', message);
+    }
+  });
+
+  setInterval(function() {
+    client.tags.get(settings.hashTag, function(err, data) {
+      if (!err) {
+        if(count < data.data.media_count) {
+          var diff = data.data.media_count - count;
+          var cnt = "";
+
+          if(diff == 1) {
+            cnt = "este o poza noua";
+          } else if(diff > 1) {
+            cnt = "sunt " + diff + " poze noi";
+          }
+
+          var message = "pe instagram " + cnt + " cu #" + settings.hashTag + "\n";
+          message += "https://www.instagram.com/explore/tags/" + settings.hashTag + "\n";
+
+          count = data.data.media_count;
+          bot.beginDialog({channel: settings.slackChannel}, '/notify', message);
+        }
+      }
+    });
+  }, 10000);
+}
+
+module.exports.init = init;
